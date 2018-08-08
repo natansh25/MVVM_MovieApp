e39a198ffc86fdd018bb6dde6fb4ff58493d4b00
Natansh Pandya <natansh25@gmail.com>
2018-08-08 15:41:20 -0400

    detail layout 50 % complete review and trailers remaining

--- app/src/main/java/com/example/natan/movietralierapp1/DetailActivity.java
+++ app/src/main/java/com/example/natan/movietralierapp1/DetailActivity.java
@@ -50,7 +50,7 @@ public class DetailActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_detail);
+        setContentView(R.layout.detailfinal);
         ButterKnife.bind(this);
         postponeEnterTransition();
 
